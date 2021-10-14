fdce6f3edd9c4e10a80db4cf67471be40f0a64a3
Leandro <leojgpereira@gmail.com>
2021-10-13 22:29:27 -0400

    Integra o Firebase para busca das informações de um produto no banco de dados

--- lib/screens/item_detalhado.dart
+++ lib/screens/item_detalhado.dart
@@ -1,15 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:patoburguer/models/item_cardapio.dart';
 
 Color corTexto = const Color(0xFF434343);
 
 class ItemDetalhado extends StatelessWidget {
-  final ItemCardapio item;
+  final String documentId;
 
-  ItemDetalhado(this.item);
+  ItemDetalhado(this.documentId);
 
   @override
   Widget build(BuildContext context) {
+    CollectionReference produtos =
+        FirebaseFirestore.instance.collection('produtos');
+
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
@@ -24,19 +28,91 @@ class ItemDetalhado extends StatelessWidget {
         elevation: 0,
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: Stack(
-        children: [
-          Align(
+      body: FutureBuilder(
+        future: produtos.doc(documentId).get(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasError ||
+              (snapshot.hasData && !snapshot.data!.exists)) {
+            return Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: double.infinity,
+                height: (MediaQuery.of(context).size.height * 0.6),
+                padding: EdgeInsets.all(25.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFFF3F3F3),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.report,
+                      size: 48.0,
+                    ),
+                    Text('Erro ao encontrar o item selecionado!')
+                  ],
+                ),
+              ),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> data =
+                snapshot.data!.data() as Map<String, dynamic>;
+
+            final ItemCardapio produto = ItemCardapio(
+              data['imagem'],
+              data['nome'],
+              data['detalhes'],
+              data['ingredientes'],
+              data['preco'],
+            );
+
+            return Stack(
+              children: [
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    width: double.infinity,
+                    height: (MediaQuery.of(context).size.height * 0.6),
+                    padding: EdgeInsets.only(
+                      top: 105.96,
+                      left: 25.0,
+                      right: 25.0,
+                      bottom: 25.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF3F3F3),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16.0),
+                        topRight: Radius.circular(16.0),
+                      ),
+                    ),
+                    child: DetalhesItem(produto),
+                  ),
+                ),
+                Positioned(
+                  top: 74.96,
+                  left: 0,
+                  right: 0,
+                  bottom:
+                      (MediaQuery.of(context).size.height * 0.5960 - 105.96),
+                  child: Image.asset(data['imagem']),
+                ),
+              ],
+            );
+          }
+
+          return Align(
             alignment: Alignment.bottomCenter,
             child: Container(
               width: double.infinity,
               height: (MediaQuery.of(context).size.height * 0.6),
-              padding: EdgeInsets.only(
-                top: 105.96,
-                left: 25.0,
-                right: 25.0,
-                bottom: 25.0,
-              ),
+              padding: EdgeInsets.all(25.0),
               decoration: BoxDecoration(
                 color: Color(0xFFF3F3F3),
                 borderRadius: BorderRadius.only(
@@ -44,17 +120,14 @@ class ItemDetalhado extends StatelessWidget {
                   topRight: Radius.circular(16.0),
                 ),
               ),
-              child: DetalhesItem(item),
+              child: Center(
+                child: CircularProgressIndicator(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
             ),
-          ),
-          Positioned(
-            top: 74.96,
-            left: 0,
-            right: 0,
-            bottom: (MediaQuery.of(context).size.height * 0.5960 - 105.96),
-            child: Image.asset(item.imagemSrc),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
